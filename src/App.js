d8b734de256d344bd4162909919f639b59ffaacc
Md. Mosabbir Hossain <rehanmosabbir@gmail.com>
2023-09-10 13:09:26 +0600

    fix some bug

--- src/App.js
+++ src/App.js
@@ -1,7 +1,79 @@
+import { useState } from "react";
 import "./App.css";
 
-function App() {
-  return <div className="App">Test</div>;
+export default function App() {
+  return (
+    <div className="App">
+      <Counter />
+    </div>
+  );
 }
 
-export default App;
+function Counter() {
+  const [step, setStep] = useState(1);
+  const [count, setCount] = useState(0);
+
+  let date = new Date();
+  date.setDate(date.getDate() + count);
+
+  function handleCountIncreament() {
+    setCount((c) => c + step);
+  }
+
+  function handleCountDecrement() {
+    setCount((c) => c - Number(step));
+  }
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (step <= 0) return;
+  }
+  function handleReset() {
+    setCount(0);
+    setStep(1);
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <input
+          type="range"
+          min="0"
+          max="10"
+          value={step}
+          onChange={(e) => setStep(Number(e.target.value))}
+        />
+        <span>{step}</span>
+      </div>
+      <br />
+      <div>
+        <button onClick={handleCountDecrement}>-</button>
+        <input
+          type="text"
+          value={count}
+          onChange={(e) => setCount(Number(e.target.value))}
+        />
+        <button onClick={handleCountIncreament}>+</button>
+      </div>
+
+      {count === 0 && <p>Today is : {date.toDateString()}</p>}
+      {count > 0 && (
+        <p>
+          {Math.abs(count)}{" "}
+          {Math.abs(count) === 1 || count === 0 ? "day" : "days"} from today is
+          : {date.toDateString()}
+        </p>
+      )}
+      {count < 0 && (
+        <p>
+          {Math.abs(count)}{" "}
+          {Math.abs(count) === 1 || count === 0 ? "day" : "days"} before today
+          is : {date.toDateString()}
+        </p>
+      )}
+      {step !== 1 || count !== 0 ? (
+        <button onClick={handleReset} type="reset">
+          reset
+        </button>
+      ) : null}
+    </form>
+  );
+}
